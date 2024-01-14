79dee28efdeaade1e6552c62883e261466c15fea
Rehanshaikh19974 <153283064+Rehanshaikh19974@users.noreply.github.com>
2024-01-14 19:58:31 +0530

    Update simple.java1

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 public class Simple {
     public static void main(String[] args) {
-        System.out.println("Welcome to Cloud Knowledge");
+        System.out.println("Welcome to Cloud Knowledges");
     }
 }
